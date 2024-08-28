d0d5645a0c93937f09b852374601e347e221b9f4
meo <1822040199@dntu.edu.vn>
2024-08-28 14:57:25 +0700

    Hoan thanh bai 02

---
+++ BaiTap28-8/BaiTap28-8/Controllers/TheLoaiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap28_8.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
