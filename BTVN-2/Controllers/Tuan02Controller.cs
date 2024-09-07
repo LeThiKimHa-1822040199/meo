0e1faba4ac58921f18282b249350b245ecb29888
meo <1822040199@dntu.edu.vn>
2024-09-07 11:41:22 +0700

    Chua xong

---
+++ BTVN-2/Controllers/Tuan02Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTVN_2.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Le Thi Kim Ha";
+            ViewBag.MSSV = "1822040199";
+            ViewData["Nam"] = "Nam 2004";
+            return View();
+        }
+    }
+}
