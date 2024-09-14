d9b8755a03d01907ce24e21b811951ec96c12985
meo <1822040199@dntu.edu.vn>
2024-09-14 15:06:01 +0700

    Hoan Thanh Bai Kiem Tra

---
+++ BaiTapKiemTra01/Controllers/TaiKhoanController.cs
@@ -0,0 +1,36 @@
+﻿using BaiTapKiemTra01.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DangKy(TaiKhoanViewModel TaiKhoan)
+        {
+            if (TaiKhoan != null && TaiKhoan.Password != null && TaiKhoan.Password.Length > 0)
+            {
+                TaiKhoan.Password = TaiKhoan.Password + "_da_ma_hoa";
+            }
+            return View(TaiKhoan);
+        }
+      
+        public IActionResult BaiTap02()
+        {
+            var sanpham = new SanPhamViewModel()
+            {
+                TenSanPham = "LapTop Gaming ABC",
+                Price = 25.000m,
+                ImageUrl = "/images/MayTinh.png" 
+
+            };
+            return View(sanpham);
+
+        }
+    }
+}
---
+++ BaiTapKiemTra01/Models/SanPhamViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BaiTapKiemTra01.Models
+{
+    public class SanPhamViewModel
+    {
+        public string TenSanPham { get; set; }
+        public decimal Price { get; set; }
+        public string HinhAnh { set; get; }
+        public string ImageUrl { get; set; }
+    }
+}
