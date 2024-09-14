23a4ee0fe2b1ed75b189f5b9dd83d68d35aacb91
meo <1822040199@dntu.edu.vn>
2024-09-14 11:51:38 +0700

    Hoan Thanh Bai Tap 06

---
+++ BaiTap06/Controllers/TaiKhoanController1.cs
@@ -0,0 +1,21 @@
+﻿using BaiTap06.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap06.Controllers
+{
+    public class TaiKhoanController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel TaiKhoan)
+        {
+            if (TaiKhoan != null && TaiKhoan.Password != null && TaiKhoan.Password.Length > 0)
+            {
+                TaiKhoan.Password = TaiKhoan.Password + "_da_ma_hoa";
+            }
+            return View(TaiKhoan);
+        }
+    }
+}
